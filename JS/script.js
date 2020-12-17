14ce6e1e7d528fe1692979b802f74d7638bc7246
Fabio Maestri <64022521+Fabiomaestri@users.noreply.github.com>
2020-12-17 01:32:17 +0000

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  /* navbar toggler */
+  $('.navbar-toggler').html("<i class='fa fa-arrow-down fa-2x' style='color:#c1ae7cff'></i>");
+
+  // smooth scroll
+
+
+});
